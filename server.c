d2791780ef6eeac7c5a435fcf0935f523e621efb
Jahnavi-pinnamaneni <dolly.pinnamaneni@gmail.com>
2022-04-10 22:20:48 -0600

    GPIO driver complete- Sprint 2

--- server.c
+++ server.c
@@ -37,6 +37,50 @@ int main()
     openlog("AESD", LOG_PERROR, LOG_USER);
 
     //create server socket
+    int get_fd;
+    struct addrinfo hints;
+    struct addrinfo *rp;
+    struct sockaddr_storage cl_addr;
+    socklen_t addr_size;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_protocol = 0;
+
+    get_fd = getaddrinfo(NULL, "9000", &hints, &rp);
+    if(get_fd != 0)
+    {
+        perror("getaddrinfo");
+        exit(1);
+    }
+
+    server_socket = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+    if(server_socket == -1)
+    {
+        perror("socket");
+        exit(1);
+    }
+    if(setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int))<0)
+        perror("setsockopt");
+
+    //bind the socket to the specified IP and port
+    if(bind(server_socket, rp->ai_addr, rp->ai_addrlen)!= 0)
+    {
+        perror("bind");
+        exit(1);
+    }
+    syslog(LOG_DEBUG, "Binded Succefully");
+    freeaddrinfo(rp);
+
+
+    //listen for connection
+    listen(server_socket, 5);
+
+    addr_size = sizeof(cl_addr);
+    client_socket = accept(server_socket, (struct sockaddr*)&cl_addr, &addr_size);
+    
     
 
     
